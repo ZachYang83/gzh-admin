4de3ddf8082839954aac3f198b5907d8441a91c7
Jay-Wuyufei <1654695333@qq.com>
2025-07-10 11:00:48 +0800

    fix common/index/js

--- src/common/index.js
+++ src/common/index.js
@@ -4,6 +4,7 @@
  * 日期 : 2020-06-01
  * 版本 : version 1.0
  */
+import { computed } from "@vue/runtime-core";
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
 import { ElMessageBox } from "element-plus";
@@ -15,26 +16,43 @@ export default function useCommon() {
     const store = useStore();
 
     const pageLoading = computed(() => store.getters.pageLoading);
-
+    /**
+     * 设置页面加载Loading
+     *
+     * @param {String} val 加载值
+     */
     function setPageLoading(val) {
         store.dispatch("app/setPageLoading", val);
     }
-
+    /**
+     * 显示开发提示框
+     */
     function showDevMessage() {
         ElMessageBox.alert("功能建设中，敬请期待", "温馨提示", {
             confirmButtonText: "确定",
         });
     }
-
+    /**
+     * 返回上一页
+     */
     function goBack() {
         router.go(-1);
     }
-
+    /**
+     * 跳转页面路径
+     * 
+     * @param {String} path 路径
+     */
     function toPage(path) {
         if (!path) return;
         router.push({ path });
     }
-
+    /**
+     * 修改配置
+     * 
+     * @param {*} key 键
+     * @param {*} value 值
+     */
     function changeSettings(key, value) {
         store.dispatch("settings/changeSetting", { key, value });
     }
